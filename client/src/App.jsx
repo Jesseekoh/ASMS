eb394be7796dec0fdd14a06df2b4afc2fe66c53f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-02-22 20:05:37 +0100

    Add Fees page and import RouterProvider from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import Layout from './layout/Layout'
-import { RouterProvider } from 'react-router-dom'
+import RequireAuth from './components/RequireAuth'
 import Dashboard from './pages/Dashboard'
 import Biodata from './pages/Biodata'
 import ErrorPage from './pages/ErrorPage'
@@ -10,8 +11,8 @@ import Register from './pages/Register'
 import Announcements from './pages/Announcements'
 import Courses from './pages/Courses'
 import Login from './pages/Login'
-import RequireAuth from './components/RequireAuth'
 import Results from './pages/Results'
+import Fees from './pages/Fees'
 
 const queryClient = new QueryClient()
 
