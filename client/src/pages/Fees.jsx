ffbcc908b487658e8df1d9e9b787a7712d700d1d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-02-23 00:59:04 +0100

    Refactor Fees and Results pages

--- client/src/pages/Fees.jsx
+++ client/src/pages/Fees.jsx
@@ -1,7 +1,47 @@
-function Fees() {
+import { useQuery } from '@tanstack/react-query'
+const Fees = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['results'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:5000/fees', {
+        credentials: 'include',
+      })
+      return response.json()
+    },
+  })
+
+  if (isLoading) {
+    return (
+      <section className="dashboard mt-12 bg-white p-4 rounded">
+        <h2 className="text-3xl font-bold pb-10">Fees</h2>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+  if (isError) {
+    return (
+      <section className="dashboard mt-12 bg-white p-4 rounded">
+        <h2 className="text-3xl font-bold pb-10">Fees</h2>
+        <p>Error fetching payments</p>
+      </section>
+    )
+  }
+  console.log(data)
   return (
     <section className="dashboard mt-12 bg-white p-4 rounded">
-      <div>Fees</div>
+      <h2 className="text-3xl font-bold pb-10">Fees</h2>
+      <div className="fees-content">
+        {data && data.fees.length > 0 ? (
+          data.fees.map((fee) => (
+            <div key={fee.name}>
+              <h3 className="font-bold">{fee.name}</h3>
+              <h3>{fee.amount}</h3>
+            </div>
+          ))
+        ) : (
+          <p>No history of payment</p>
+        )}
+      </div>
     </section>
   )
 }
--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,7 +1,7 @@
 const Results = () => {
   return (
     <section className="dashboard mt-12 bg-white p-4 rounded">
-      <div>Results</div>
+      <h2 className="text-3xl font-bold pb-10">Results</h2>
     </section>
   )
 }
