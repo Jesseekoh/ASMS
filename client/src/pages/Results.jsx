538e5b041c7e98c38ee0a84a8a50efb5527ddbf2
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-02-26 15:33:40 +0100

    Add useQuery hook and fetchResults function to Results component

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,11 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchResults } from '../helper'
+
 /**
  * This component represents the results page.
  * @returns {ReactNode} a results page
  */
 const Results = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['results'],
+    queryFn: fetchResults,
+  })
+
+  if (data) console.log(data)
   return (
     <section className="dashboard mt-12 bg-white p-4 rounded">
       <h2 className="text-3xl font-bold pb-10">Results</h2>
+      {/* {Array.isArray(data) && (data.map(result => ()))} */}
     </section>
   )
 }
